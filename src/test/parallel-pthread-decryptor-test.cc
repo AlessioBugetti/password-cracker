5218dfe738549a3337ffeff4b0a960e026c518fd
AlessioBugetti <alessiobugetti98@gmail.com>
2024-10-11 16:14:52 +0200

    Refactor tests

--- src/test/parallel-pthread-decryptor-test.cc
+++ src/test/parallel-pthread-decryptor-test.cc
@@ -9,16 +9,17 @@
 #include <memory>
 #include <unistd.h>
 
+#define SALT "pc"
+
 using namespace passwordcracker;
 
 TEST(ParallelPthreadDecryptorTest, DecryptSuccess)
 {
     std::vector<std::string> passwords = {"acqua", "vento", "aria", "fuoco"};
     std::string password = "vento";
-    std::string salt = "pc";
     auto decryptor = std::make_unique<ParallelPThreadDecryptor>(passwords);
 
-    std::string encryptedPassword = crypt(password.c_str(), salt.c_str());
+    std::string encryptedPassword = crypt(password.c_str(), SALT);
     auto [found, decryptedPassword, time] = decryptor->Decrypt(encryptedPassword);
 
     EXPECT_TRUE(found);
@@ -29,10 +30,9 @@ TEST(ParallelPthreadDecryptorTest, DecryptFailure)
 {
     std::vector<std::string> passwords = {"acqua", "vento", "aria", "fuoco"};
     std::string password = "terra";
-    std::string salt = "pc";
     auto decryptor = std::make_unique<ParallelPThreadDecryptor>(passwords);
 
-    std::string encryptedPassword = crypt(password.c_str(), salt.c_str());
+    std::string encryptedPassword = crypt(password.c_str(), SALT);
     auto [found, decryptedPassword, time] = decryptor->Decrypt(encryptedPassword);
 
     EXPECT_FALSE(found);
--- src/test/sequential-decryptor-test.cc
+++ src/test/sequential-decryptor-test.cc
@@ -9,26 +9,30 @@
 #include <memory>
 #include <unistd.h>
 
+#define SALT "pc"
+
 using namespace passwordcracker;
 
 TEST(SequentialDecryptorTest, DecryptSuccess)
 {
     std::vector<std::string> passwords = {"acqua", "vento", "aria", "fuoco"};
+    std::string password = "vento";
     auto decryptor = std::make_unique<SequentialDecryptor>(passwords);
 
-    std::string encryptedPassword = crypt("aria", "pc");
+    std::string encryptedPassword = crypt(password.c_str(), SALT);
     auto [found, decryptedPassword, time] = decryptor->Decrypt(encryptedPassword);
 
     EXPECT_TRUE(found);
-    EXPECT_EQ(decryptedPassword, "aria");
+    EXPECT_EQ(decryptedPassword, password);
 }
 
 TEST(SequentialDecryptorTest, DecryptFailure)
 {
     std::vector<std::string> passwords = {"acqua", "vento", "aria", "fuoco"};
+    std::string password = "terra";
     auto decryptor = std::make_unique<SequentialDecryptor>(passwords);
 
-    std::string encryptedPassword = crypt("terra", "pc");
+    std::string encryptedPassword = crypt(password.c_str(), SALT);
     auto [found, decryptedPassword, time] = decryptor->Decrypt(encryptedPassword);
 
     EXPECT_FALSE(found);
